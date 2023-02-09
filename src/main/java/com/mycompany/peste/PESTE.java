22737f268e7b549bf02ed647c3c7de53f0764d4f
josep <josep@DESKTOP-NJHN7RD>
2023-02-08 19:32:28 -0600

    cAMBIOS

---
+++ src/main/java/com/mycompany/peste/PESTE.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.peste;
+
+/**
+ *
+ * @author josep
+ */
+public class PESTE {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
